40e852c53c204cbdccbd753187afdd3ddd1768ad
AfrahSaud36 <afrahsaud36@gmail.com>
2025-07-03 14:13:55 +0300

    Initial Commit

---
+++ ShohnaPlusNIDLP/ShohnaPlusNIDLPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShohnaPlusNIDLPApp.swift
+//  ShohnaPlusNIDLP
+//
+//  Created by Afrah Alharbi on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShohnaPlusNIDLPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShohnaPlusNIDLPTests/ShohnaPlusNIDLPTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShohnaPlusNIDLPTests.swift
+//  ShohnaPlusNIDLPTests
+//
+//  Created by Afrah Alharbi on 03/07/2025.
+//
+
+import Testing
+@testable import ShohnaPlusNIDLP
+
+struct ShohnaPlusNIDLPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
